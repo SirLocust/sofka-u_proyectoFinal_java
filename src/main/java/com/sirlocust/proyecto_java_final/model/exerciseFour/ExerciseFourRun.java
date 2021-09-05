3daba2af0b5b5bd80888d19d48dd2d37c0384c5e
sirlocust <jua56yu@hotmail.com>
2021-09-04 21:46:13 -0500

    se crear la clase principal para correr este ejercisio

--- src/main/java/com/sirlocust/proyecto_java_final/model/exerciseFour/ExerciseFourRun.java
+++ src/main/java/com/sirlocust/proyecto_java_final/model/exerciseFour/ExerciseFourRun.java
@@ -1,5 +1,16 @@
 package com.sirlocust.proyecto_java_final.model.exerciseFour;
 
-public class ExerciseFourRun {
+import com.sirlocust.proyecto_java_final.model.exerciseFour.controller.ExerciseFourController;
+import com.sirlocust.proyecto_java_final.model.exerciseFour.model.CalculateProductWithPrice;
+import com.sirlocust.proyecto_java_final.model.exerciseFour.view.ExerciseFourDashboard;
 
+public class ExerciseFourRun {
+  public boolean run() {
+    ExerciseFourDashboard exerciseFourDashboard = new ExerciseFourDashboard();
+    CalculateProductWithPrice calculateProductWithPrice = new CalculateProductWithPrice();
+    ExerciseFourController exerciseFourController = new ExerciseFourController(exerciseFourDashboard,
+        calculateProductWithPrice);
+
+    return exerciseFourController.init();
+  }
 }
