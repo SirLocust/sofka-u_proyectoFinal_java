317548e7937be2801babd14eaf0942613065cb60
sirlocust <jua56yu@hotmail.com>
2021-09-05 15:55:22 -0500

    se crean los test y el mentodo para la solucion con for

--- src/main/java/com/sirlocust/proyecto_java_final/model/exerciseFive/model/NumbersOddAndEven.java
+++ src/main/java/com/sirlocust/proyecto_java_final/model/exerciseFive/model/NumbersOddAndEven.java
@@ -5,10 +5,23 @@ import java.util.List;
 
 public class NumbersOddAndEven {
 
-  public List<List<Integer>> generateOddAndEvenWhiel() {
+  public List<List<Integer>> generateOddAndEvenWhile() {
     List<List<Integer>> lista = this.generateList();
     Integer count = 0;
-    while (count >= 100) {
+    while (count <= 100) {
+      if ((count % 2) == 0) {
+        lista.get(0).add(count);
+      } else {
+        lista.get(1).add(count);
+      }
+      count++;
+    }
+    return lista;
+  }
+
+  public List<List<Integer>> generateOddAndEvenFor() {
+    List<List<Integer>> lista = this.generateList();
+    for (int count = 0; count < 101; count++) {
       if ((count % 2) == 0) {
         lista.get(0).add(count);
       } else {
@@ -24,4 +37,5 @@ public class NumbersOddAndEven {
     lista.add(new ArrayList<>());
     return lista;
   }
+
 }
