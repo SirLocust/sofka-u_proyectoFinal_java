8dee05a61d7ae8554ff53e101c38d0e96ca1b489
sirlocust <jua56yu@hotmail.com>
2021-09-09 15:18:02 -0500

    se crea el ejercisio 10 con sus paquetes y test

---
+++ src/main/java/com/sirlocust/proyecto_java_final/model/exerciseTen/ExerciseTenRun.java
@@ -0,0 +1,18 @@
+package com.sirlocust.proyecto_java_final.model.exerciseTen;
+
+import com.sirlocust.proyecto_java_final.model.exerciseTen.controller.ExerciseTenController;
+import com.sirlocust.proyecto_java_final.model.exerciseTen.model.RemoveSpacesFromPhrase;
+import com.sirlocust.proyecto_java_final.model.exerciseTen.view.ExerciseTenDashboard;
+
+public class ExerciseTenRun {
+
+  public boolean run() {
+
+    ExerciseTenDashboard exerciseTenDashboard = new ExerciseTenDashboard();
+    RemoveSpacesFromPhrase removeSpacesFromPhrase = new RemoveSpacesFromPhrase();
+    ExerciseTenController exerciseTenController = new ExerciseTenController(exerciseTenDashboard,
+        removeSpacesFromPhrase);
+    return exerciseTenController.init();
+  }
+
+}
